f23ca84a93fa96cf75408e6662e18f24a5b535a9
volochayev <stingray8888831@gmail.com>
2025-06-05 23:52:30 +0300

    закінчив перше завдання

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -27,10 +27,69 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        const selectedDates = selectedDates[0];
+        const selectedDate = selectedDates[0];
 
-        if ()
-      
+        if (selectedDate <= new Date()) {
+            iziToast.warning({
+                title: 'Warning',
+                message: 'Please choose a date in the future',
+                position: 'topRight',
+            });
+            startBtn.disabled = true;
+        } else {
+            userSelectedDate = selectedDate;
+            startBtn.disabled = false;
+        }
     },
-  };
+      
+};
   
+flatpickr(datetimeInput, options);
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+}
+function updateTimer({ days, hours, minutes, seconds }) {
+    daysEl.textContent = days;
+    hoursEl.textContent = addLeadingZero(hours);
+    minutesEl.textContent = addLeadingZero(minutes);
+    secondsEl.textContent = addLeadingZero(seconds);
+}
+function startTimer() {
+    timerId = setInterval(() => {
+      const now = new Date();
+      const delta = userSelectedDate - now;
+      if (delta <= 0) {
+        clearInterval(timerId);
+        updateTimer(convertMs(0));
+        datetimeInput.disabled = false;
+        startBtn.disabled = true;
+        return;
+      }
+      updateTimer(convertMs(delta));
+    }, 1000);
+}
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    datetimeInput.disabled = true;
+    startTimer();
+  });
